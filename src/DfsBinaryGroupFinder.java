e32b67eae7f372da077537d6c30e37d4ffa3737d
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-04-30 14:20:58 -0700

    Adjustments to sortDesc method

--- src/DfsBinaryGroupFinder.java
+++ src/DfsBinaryGroupFinder.java
@@ -61,27 +61,115 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
             }
         }
         // sortDesc(totalGroups);
-
+        System.out.println(totalGroups);
         return sortDesc(totalGroups);
     }
 
     public static List<Group> sortDesc(List<Group> group){
-        if (group.isEmpty() || group == null) throw new IndexOutOfBoundsException(); 
+        if (group.isEmpty()) return new ArrayList<>(); 
         // System.err.println(group.size());
-        List<Group> sorted = new ArrayList<>();
-        Group largestGroup = group.get(0);
-        
-        for(Group g: group){
-            if (g.compareTo(largestGroup) > 0) {
-                sorted.add(0, g);
-                largestGroup = g;
-            } else {
-                sorted.add(g);
-            }
-        }
-        return sorted;
-    }
+        List<Group> sorted = group;
+        // System.out.println("this is the entire size" + sorted.size());
+        // System.out.println(sorted.get(0));
+        // System.out.println(sorted.get(1));
+        // System.out.println();
+        System.out.println();
+        for(int i = 0; i < sorted.size(); i++){
+            
+            Group current = sorted.get(i); 
+            // System.out.println("current " + current);
+            
+            for(int j = 0; j < sorted.size() - 1; j++){
+                
+                Group next = sorted.get(j + 1);
+                // System.out.println("next " + next);
+                int comp = current.compareTo(next);
+                // System.out.println("comparing:" + current + " to " + next);
+                // System.out.println("comp " + comp);
+                // System.out.println();
 
+                if(comp == 0){
+                    continue;
+                }
+                
+                if(comp < 0){
+                    sorted.set(i, next);
+                    // System.out.println("current sorting: " + sorted);
+                    sorted.set(j + 1, current);
+                    current = next;
+                    // System.out.println("current sorting: " + sorted);
+                    // System.out.println(current + " is less than " + next);
+                }
+
+                /*
+                    [Group[size=3, centroid=Coordinate[x=2, y=0]], Group[size=1, centroid=Coordinate[x=0, y=0]],  Group[size=1, centroid=Coordinate[x=0, y=2]]]
+                */
+
+                if (comp > 0) {
+                    continue;
+                }
+
+                
+            }
+
+            /*
+                    int[][] image = {
+                        {1, 0, 1, 1},
+                        {0, 0, 0, 1},
+                        {1, 0, 0, 0}
+                };
+            */
+        }
+
+        return sorted;
+
+        /*
+            if group is less than or greater return otherwise if theyre equal
+            compare their x if it is less than or greater return otherwise if these are equal
+            compare their y return whether it is less or greater than or equal
+
+            we need o(n^2) time complexity
+            group(0) to group(1) we want this comparison
+
+            inner for loop
+            check(this.compareTo(other)) > < = (1) (-1) (0) the other group 
+                if our current is greater than our current[0]
+                
+                [1, 2, 3, 4, 5, 6, 7]
+
+                1.compare(2) returns -1 swap
+
+                [2, 1, 3, 4, 5, 6, 7]
+
+                1.compare(1) continue
+
+                [2, 1, 3, 4, 5, 6, 7]
+
+                1.compare(3) returns -1 swap
+
+                [2, 3, 1, 4, 5, 6, 7]
+
+                ...
+                [2, 3, 4, 5, 6, 7, 1]
+                ...
+                [3, 4, 5, 6, 7, 2, 1]
+                ...
+                [4, 5, 6, 7, 3, 2, 1]
+                ...
+                [5, 6, 7, 4, 3, 2, 1]
+                ...
+                [5, 6, 7, 4, 3, 2, 1]
+                ...
+                [6, 7, 5, 4, 3, 2, 1]
+                ...
+                [7, 6, 5, 4, 3, 2, 1]
+
+
+
+
+
+        */
+    }
     // rSize 3 cSize 4  newRow = 2 newCol = 0  result = [[1,3], [2, 3], [2, 2], [2, 1], [2, 0]]
     // movements down (+1, 0)
     public void dfs(int[][] image, int newRow, int newCol, boolean[][] visited, List<Coordinate> result) {
@@ -98,16 +186,29 @@ public class DfsBinaryGroupFinder implements BinaryGroupFinder {
     
     public static void main(String[] args) {
         DfsBinaryGroupFinder finder = new DfsBinaryGroupFinder();
-        int[][] image = {
-                {1, 1, 0, 0},
-                {0, 0, 0, 1},
-                {1, 1, 1, 1}
-        };
+        // int[][] image = {
+        //         {1, 1, 0, 0},
+        //         {0, 0, 0, 1},
+        //         {1, 1, 1, 1}
+        // };
 
+        // int[][] image = {
+        //         {1, 0, 1, 1},
+        //         {0, 0, 0, 1},
+        //         {1, 0, 0, 0}
+        // };
+
+        int[][] image = {
+                {1, 0, 1},
+                {0, 0, 0},
+                {1, 0, 1}
+        };
         // System.out.println(finder.findConnectedGroups(image));
 
         // sortDesc(finder.findConnectedGroups(image));
+        ;
 
-        System.out.println(sortDesc(finder.findConnectedGroups(image)));
+        System.out.println(finder.findConnectedGroups(image));
+        // System.out.println(sortDesc(finder.findConnectedGroups(image)));
     }
 }
