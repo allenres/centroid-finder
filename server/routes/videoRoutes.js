b29bc88304ef58f912eedddc025418de12930204
allenres <allen.resulidze@gmail.com>
2026-05-25 15:51:44 -0700

    fix typo in videoController

--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/api/videos', videoController.listVideos); 
 router.get('/thumbnail/:filename', videoController.getThumbnail); 
-router.post('/process/:filename', videoControllerideoController.createJob); 
+router.post('/process/:filename', videoController.createJob); 
 router.get('/process/:jobId/status', videoController.checkJobStatus);
 
 export default router;
