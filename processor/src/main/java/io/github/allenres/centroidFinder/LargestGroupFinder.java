20e9a85c43051ace42cc5294082155bbc5430e0b
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-06-03 12:15:42 -0700

    Added Javadocs to LargestGroupFinder

--- processor/src/main/java/io/github/allenres/centroidFinder/LargestGroupFinder.java
+++ processor/src/main/java/io/github/allenres/centroidFinder/LargestGroupFinder.java
@@ -3,10 +3,21 @@ package io.github.allenres.centroidFinder;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-    Return the largest group.
-    Return null if empty.
-*/
+/**
+ * Finds the largest group from a list of connected pixel groups.
+ *
+ * The "largest" group is determined by the size of the group, where size
+ * represents the number of pixels contained in that group.
+ *
+ * If multiple groups have the same maximum size, the last encountered group
+ * in the list is selected due to the use of a greater-than-or-equal comparison.
+ *
+ * If the input list is empty, a default Group is returned with size equal to
+ * 0 and a centroid of (-1, -1), indicating that no valid group was found.
+ *
+ * @param groups a list of connected pixel groups to evaluate
+ * @return the largest Group based on size, or a default Group if the list is empty
+ */
 public class LargestGroupFinder {
     public Group findLargest(List<Group> groups) {
         if (groups.isEmpty()) return new Group(groups.size(), new Coordinate(-1, -1));
